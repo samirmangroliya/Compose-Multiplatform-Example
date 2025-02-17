4c575d34effb25d75888cb42fa1c2f536f6ad657
Samir Mangroliya <samir.mangroliya@zoodmall.com>
2025-02-17 16:49:37 +0530

    Compose multiplatform project creation

---
+++ composeApp/src/desktopMain/kotlin/com/samir/cmpapicalimageload/main.kt
@@ -0,0 +1,13 @@
+package com.samir.cmpapicalimageload
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "CMP-ApiCall-ImageLoad",
+    ) {
+        App()
+    }
+}
